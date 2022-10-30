439ea2b0336154b08bc3ddc8755bd119647b4461
yyakovlev <yury.yakovlev@gmail.com>
2022-10-31 00:02:33 +0300

    refer to Bill Gates

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -3,7 +3,8 @@ package com.sample;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        System.out.println("Hello there Bill Gates");
+        //Email him at bill.gates@microsoft.com
     }
 
 }

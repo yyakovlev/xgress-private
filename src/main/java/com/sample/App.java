172546d0cfb4322f9117762aa7737541d87c9897
yyakovlev <yury.yakovlev@gmail.com>
2022-11-06 08:00:12 +0300

    Masked changes for initial egress 2022-10-25

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -3,8 +3,8 @@ package com.sample;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello there Bill Gates");
-        //Email him at bill.gates@microsoft.com
+        System.out.println("Hello there William w1195608576");
+        //Email him at u582083870@mail.local
     }
 
 }
